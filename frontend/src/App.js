3e1da42c1a48e1e469edf12fc47a270eb26b5efc
topmcon <admin@topmcon.com>
2025-11-29 23:45:23 +0000

    Redirect login page to dashboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -29,7 +29,7 @@ function App() {
       <Router>
         <div className="App">
           <Routes>
-            <Route path="/login" element={<Login />} />
+            <Route path="/login" element={<Navigate to="/" />} />
             <Route
               path="/*"
               element={
